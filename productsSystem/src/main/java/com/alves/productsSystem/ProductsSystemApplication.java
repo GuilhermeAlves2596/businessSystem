ad1724baabdeb5c687703485bf79cbdc0fdcd52b
GuilhermeAlves2596 <112896210+GuilhermeAlves2596@users.noreply.github.com>
2024-01-05 11:26:53 -0300

    Add files via upload

---
+++ productsSystem/src/main/java/com/alves/productsSystem/ProductsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alves.productsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsSystemApplication.class, args);
+	}
+
+}
